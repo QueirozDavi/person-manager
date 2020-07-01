a9ca5d5a2411fdf6510975f429cdd102afd3a79f
Davi <Queiroz>
2020-07-01 12:15:53 -0300

    adicionando PersonDTO

--- manager/src/main/java/com/personmanager/manager/domain/dto/PersonDTO.java
+++ manager/src/main/java/com/personmanager/manager/domain/dto/PersonDTO.java
@@ -1,4 +1,35 @@
 package com.personmanager.manager.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PersonDTO {
+
+    private Long id;
+
+    private String name;
+
+    private char gender;
+
+    @Email
+    private String email;
+
+    @NotNull
+    private String birthDate;
+
+    @CPF
+    @Size(max = 11, min = 11)
+    @NotNull
+    private String cpf;
+
+    private String birthPlace;
+
+    private String nationality;
 }
